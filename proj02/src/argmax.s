4c1e83a19eb442231a01a5f985d2cfb9f2bfabeb
TheForsaking <1484636586@qq.com>
2024-03-14 20:48:47 +0800

    done proj02 part A

--- proj02/src/argmax.s
+++ proj02/src/argmax.s
@@ -17,18 +17,44 @@
 argmax:
 
     # Prologue
+    li t1, 1
+    blt a1, t1, exit_57
 
-
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    
+    mv t0, a0
+    mv t3, a0
+    slli t1, a1, 2
+    add t1, t1, a0
+    lw s1, 0(a0)
+    addi t0, t0, 4
 loop_start:
-
-
+    bge t0, t1, loop_end
+    lw s0, 0(t0)
+    blt s1, s0, update
+    j loop_continue
+update:
+    mv a0, t0
+    lw s1, 0(a0)
 loop_continue:
-
-
+    addi, t0, t0, 4
+    j loop_start
 loop_end:
-
+    sub a0, a0, t3
+    srai a0, a0, 2
 
     # Epilogue
+    lw s2, 8(sp)
+    lw s1, 4(sp)
+    lw s0, 0(sp)
+    addi sp, sp, 12
 
 
     ret
+
+exit_57:
+    li a1, 57
+    call exit2
